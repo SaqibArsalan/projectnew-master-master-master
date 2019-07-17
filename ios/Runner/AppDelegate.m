1c7a09b2ae8b44587a214fe4a92d58bf67dc304f
Saqib Arsalan Ijaz <45181181+SaqibArsalan@users.noreply.github.com>
2019-07-17 14:37:48 +0500

    Update AppDelegate.m

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -7,7 +7,7 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-  [GMSServices provideAPIKey:@"AIzaSyC_2vdelF5OBkFaphMp265a44jVwcF_eYI"];
+ 
   [GeneratedPluginRegistrant registerWithRegistry:self];
     // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
